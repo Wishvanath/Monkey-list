455101f78bd51c31590f8f503e6e2454e59881b7
Wishvanath SMG <anandwishvanath@gmail.com>
2020-01-02 18:02:23 +0530

    Displayed monkey list using rest api and added search filter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import MonkeyList from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      This is app component React
-    </div>
-  );
+export default class App extends Component {
+  // define constructor
+  constructor(){
+    super();
+    this.state = {
+
+
+      // monekey list arrary
+      monkeys: [],
+      searchField: ''
+
+    }
+  }
+
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => this.setState({monkeys: data}))
+  }
+
+
+
+
+
+  // render main component
+  render() {
+    // destructoring the state variable
+    const {monkeys , searchField} = this.state;
+    // flilteration of monkey on set the state
+    const filteredMonkey = monkeys.filter(monkey => 
+      monkey.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+    )
+    return (
+      <div>
+
+        <input  type ="search" placeholder ="search Monkey" onChange={(e) => {
+          // console.log(e.target.value);
+          this.setState({searchField: e.target.value})
+        }}/>
+
+        {/* {JSON.stringify(searchField)} */}
+        <MonkeyList data ={filteredMonkey}> 
+           {/* {this.state.monkey.map((monkey_data) =>(
+            <h2 key={monkey_data.id}>{monkey_data.name}</h2>
+           ))} */}
+        </MonkeyList>
+
+        {/* show all the monkey list below */}
+        {/* {this.state.monkey.map((monkey_data) =>(
+          <h1 key={monkey_data.id}>{monkey_data.name}</h1>
+        ))} */}
+
+        {/* <MonkeyList name = {this.state.monkey}/> */}
+
+      </div>
+    )
+  }
 }
-
-export default App;
